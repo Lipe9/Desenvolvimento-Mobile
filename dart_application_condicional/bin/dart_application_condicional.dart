30aa9cffaa12bfaf7a911e0ac8771f4413345ead
Fellipe ツ <antonio.pereira312@aluno.ce.gov.br>
2024-02-22 09:38:39 -0300

    Add files via upload

---
+++ dart_application_condicional/bin/dart_application_condicional.dart
@@ -0,0 +1,37 @@
+
+void main(){
+  var grade = "B";
+
+  switch(grade){
+    case "A":
+    {
+      print("Excellent");
+    }
+    break;
+    case "B":
+    {
+      print("Muy bien");
+    }
+    break;
+    case "C":
+    {
+      print("Regular");
+    }
+    break;
+     case "D":
+    {
+      print("Ruim");
+    }
+    break;
+     case "E":
+    {
+      print("Péssimo");
+    }
+    break;
+    default:{
+      print("Nota invalida");
+    }
+  }
+}
+
+DESENVOLVIMENTO DE APLICAÇÕES PARA DISPOSITIVOS MÓVEIS
---
+++ dart_application_condicional/test/dart_application_condicional_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_condicional/dart_application_condicional.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
